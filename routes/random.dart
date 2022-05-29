ec7f37d1176db8213064a7a1c05141716b010918
João Victor Reis Alves <joao.reisa13@gmail.com>
2022-05-29 15:26:40 -0300

    Add Random Quote route

--- routes/random.dart
+++ routes/random.dart
@@ -1,9 +1,20 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:dart_frog/dart_frog.dart';
 
 import '../json/fixture_reader.dart';
 
 Response onRequest(RequestContext context) {
-  return Response.json(body: jsonDecode(fixture('quotes.json')));
+  final response = randomize();
+  return Response.json(body: response);
+}
+
+Map<String, dynamic> randomize() {
+  final dynamic quotes = json.decode(fixture('quotes.json'));
+  final rng = Random();
+  final dynamic randomQuote = quotes[rng.nextInt(quotes.length as int)];
+  final map = {'quote': '$randomQuote'};
+
+  return map;
 }
